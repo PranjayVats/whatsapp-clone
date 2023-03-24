64e7faef041b9f4391287071066dd33a95b98cc7
Pranjay Vats <pranjayvats22062001@gmail.com>
2023-03-24 16:26:02 +0530

    changes commit

--- frontend/src/Axios.js
+++ frontend/src/Axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "https://silver-peony-9c96d4.netlify.app/.netlify/functions/server",
+    baseURL: "https://whatsapp-clone-node-server.netlify.app/.netlify/functions/server",
 });
 export default instance;
